661c0c7cb3b4d524af0c80f6ae1b545a2a9d21e7
tsubramanian <tamilselvan@gmail.com>
2023-08-28 14:22:10 +0530

    Update __init__.py

--- wbia-whaleridgefindr/__init__.py
+++ wbia-whaleridgefindr/__init__.py
@@ -2,6 +2,6 @@
 try:
     from wbia-whaleridgefindr._version import __version__
 except ImportError:
-    __version__ = '0.0.0'
+    __version__ = '0.1.1'
 
 from wbia-whaleridgefindr import _plugin  # NOQA
