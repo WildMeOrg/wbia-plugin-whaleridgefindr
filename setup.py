42a6ea83d7c4fce10191995c3b81622137090056
tsubramanian <tamilselvan@gmail.com>
2023-08-28 11:03:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -203,7 +203,7 @@ KWARGS = OrderedDict(
     # See https://github.com/pypa/setuptools_scm/ for more information
     setup_requires=['setuptools_scm'],
     use_scm_version={
-        'write_to_template': '__version__ = "0.0.2"',
+        'write_to_template': '__version__ = "0.2"',
         'tag_regex': '^(?P<prefix>v)?(?P<version>[^\\+]+)(?P<suffix>.*)?$',
         'local_scheme': 'dirty-tag',
     },
